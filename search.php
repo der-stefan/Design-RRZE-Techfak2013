dc8c877f58a01ce6df2e85f204c94147a1e5766e
Barbara Bothe <barbara.bothe@fau.de>
2013-07-16 14:52:08 +0200

    Initial Commit

---
+++ search.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The template for displaying Search Results pages.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+<?php get_sidebar(); ?>
+
+	<section id="primary" class="site-content">
+		
+	<div class="breadcrumbs">
+	    <?php if(function_exists('bcn_display'))
+	    {
+	        bcn_display();
+	    }?>
+	</div>
+	
+	<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'techfak-2013' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			</header>
+
+			<?php twentytwelve_content_nav( 'nav-above' ); ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
+
+			<?php twentytwelve_content_nav( 'nav-below' ); ?>
+
+		<?php else : ?>
+
+			<article id="post-0" class="post no-results not-found">
+				<header class="entry-header">
+					<h1 class="entry-title"><?php _e( 'Nothing Found', 'techfak-2013' ); ?></h1>
+				</header>
+
+				<div class="entry-content">
+					<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'techfak-2013' ); ?></p>
+					<?php get_search_form(); ?>
+				</div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php get_footer(); ?>
